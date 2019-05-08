ff7580b5618652ae2505143c339ace5ad0db3cc8
Dani Garcia Sanchez <dgarcia@fluendo.com>
2019-05-08 19:15:57 +0200

    Add simple animation

---
+++ Assets/RotateOverTime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTime : MonoBehaviour
+{
+    public bool RotateXAxis;
+    public bool RotateYAxis;
+    public bool RotateZAxis;
+    public float AngularSpeed = 1;
+
+
+    private float currentXDegree;
+    private float currentYDegree;
+    private float currentZDegree;
+    // Update is called once per frame
+    void Update()
+    {
+        if(RotateXAxis)
+        {
+            currentXDegree = (AngularSpeed * Time.deltaTime) % 360;
+        }
+
+        if(RotateYAxis)
+        {
+            currentYDegree = (AngularSpeed * Time.deltaTime) % 360;
+        }
+
+        if(RotateZAxis)
+        {
+            currentZDegree = (AngularSpeed * Time.deltaTime) % 360;
+        }
+
+        gameObject.transform.Rotate(currentXDegree, currentYDegree, currentZDegree);
+    }
+}
