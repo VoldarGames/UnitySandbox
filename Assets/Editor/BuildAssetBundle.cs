e44a3ade9e1317115c3a03a59e94dc96e0ab95c9
Dani Garcia Sanchez <dgarcia@fluendo.com>
2019-05-08 18:12:27 +0200

    First commit

---
+++ Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildAssetBundle : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundles/Normal")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/Features/AssetBundles/BuildFolder";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("Assets/Build AssetBundles/Strict")]
+    static void BuildAllAssetBundlesStrict()
+    {
+        string assetBundleDirectory = "Assets/Features/AssetBundles/BuildFolder";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        var manifest = BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.StrictMode, BuildTarget.StandaloneWindows);
+        var bundleNamesCSV = StringArrayToCSV(manifest.GetAllAssetBundles());
+        File.WriteAllText(assetBundleDirectory + "/AssetBundleNames.csv", bundleNamesCSV);
+    }
+
+    static string StringArrayToCSV(string[] stringArray)
+    {
+        var result = string.Empty;
+        foreach (var str in stringArray)
+        {
+            result += string.Format("{0},", str);
+        }
+        return result.TrimEnd(',');
+    }
+}
---
+++ Assets/Features/AssetBundles/AssetLoader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class AssetLoader : MonoBehaviour
+{
+    static List<string> assetBundleList = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitAssetBundleList();
+        InstantiateRandomAssetBundleGameObject("Shape");
+        InstantiateRandomAssetBundleGameObject("Ornament");
+        InstantiateRandomAssetBundleGameObject("Bottom");
+        InstantiateRandomAssetBundleGameObject("Structure");
+    }
+
+    static void InstantiateRandomAssetBundleGameObject(string prefixLocation)
+    {
+        prefixLocation = prefixLocation.ToLower();
+        var prefixAssetList = assetBundleList.Where(s => s.StartsWith(prefixLocation)).ToArray();
+        var prefixAssetListCount = prefixAssetList.Count();
+        int randomIndex = 0;
+        if(prefixAssetListCount > 1)
+        {
+            randomIndex = UnityEngine.Random.Range(0, prefixAssetListCount);
+        }
+        
+        var loadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, prefixAssetList[randomIndex]));
+        if (loadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        var names = loadedAssetBundle.GetAllAssetNames();
+        var prefab = loadedAssetBundle.LoadAsset<GameObject>(names[0]);
+        Instantiate(prefab, new Vector3(2, 0, 0), Quaternion.identity);
+    }
+
+    void InitAssetBundleList()
+    {
+        var assetBundleNamesCSV = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, "AssetBundleNames.csv"));
+
+        if(string.IsNullOrEmpty(assetBundleNamesCSV))
+        {
+            Debug.LogError("AssetBundleNames.csv can't be found on StrimgAssets folder");
+        }
+
+        foreach (var name in assetBundleNamesCSV.Split(','))
+        {
+            assetBundleList.Add(name);
+        }
+    }
+}
